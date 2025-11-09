90e7a4dd46d0a39fc0d7c2495a3f3dc6bc0ddbe6
geekbazar <geekbazar47@gmail.com>
2025-11-09 14:48:25 +0530

    test

--- urbanmakaan.api/urbanmakaan.api.AppHost/AppHost.cs
+++
@@ -1,6 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.urbanmakaan_api_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
-
-builder.Build().Run();
